02c0d89fe6ae960b9feabb3683b949a7246a4b35
João Victor <dev.joaovictor@hotmail.com>
2026-04-02 13:51:47 +0100

    update sqs config adding dlq and redrive policy

--- infra/modules/sqs/main.tf
+++ infra/modules/sqs/main.tf
@@ -1,14 +1,37 @@
-resource "aws_sqs_queue" "sqs" {
-  name = var.queue_name
-  create_dlq = true
-
-  redrive_policy = {
-    maxReceiveCount = 5
-  }
+resource "aws_sqs_queue" "dlq" {
+  name = "${var.queue_name}_dlq"
+  message_retention_seconds = 1209600
+  sqs_managed_sse_enabled   = true
 
   tags = {
     FromTerraform = true
     Environment = terraform.workspace
     Project = var.project
   }
+}
+
+resource "aws_sqs_queue" "sqs" {
+  name = var.queue_name
+  message_retention_seconds = 345600
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = 3 # Moves to DLQ after 3 failures
+  })
+
+
+  tags = {
+    FromTerraform = true
+    Environment = terraform.workspace
+    Project = var.project
+  }
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "example" {
+  queue_url = aws_sqs_queue.dlq.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.sqs.arn]
+  })
 }
